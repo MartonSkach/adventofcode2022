6ac214b77939ba6883e8a386cce064244f2f5793
Marton Skach <martonskach@gmail.com>
2022-12-04 21:30:46 +0100

    Add solution for day 4

---
+++ day4/part1.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+
+const example = fs.readFileSync("example.txt").toString().split(/\r?\n/);
+const input = fs.readFileSync("input.txt").toString().split(/\r?\n/);
+
+let total = 0;
+let array = input;
+
+array.forEach(pair => {
+  let i1 = pair.split(',')[0];
+  let i2 = pair.split(',')[1];
+  let a1 = [];
+  let a2 = [];
+
+  for (let i = parseInt(i1.split('-')[0]); i <= parseInt(i1.split('-')[1]); i++) {
+    a1.push(i)
+  }
+  for (let j = parseInt(i2.split('-')[0]); j <= parseInt(i2.split('-')[1]); j++) {
+    a2.push(j)
+  }
+
+  if (a1.every(e => a2.includes(e)) || a2.every(e => a1.includes(e))) {
+    total++
+  }
+})
+
+console.log(total);
